6b39e52a98f65a47c59ecc2b2d18c90cc6ef9461
RenYongXiang <2673741645@qq.com>
2024-08-25 17:15:56 +0800

    add notes

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -25,7 +25,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
     nav: [
       { text: '首页', link: '/' },
       {
-        text: '前端',
+        text: '编程',
         link: '/web/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
         items: [
           // 说明：以下所有link的值只是在相应md文件头部定义的永久链接（不是什么特殊编码）。另外，注意结尾是有斜杠的
@@ -64,7 +64,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
         ],
       },
       {
-        text: '页面',
+        text: '英语',
         link: '/ui/',
         items: [
           { text: 'HTML', link: '/pages/8309a5b876fc95e3/' },
@@ -72,7 +72,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
         ],
       },
       {
-        text: '技术',
+        text: '数学',
         link: '/technology/',
         items: [
           { text: '技术文档', link: '/pages/9a7ee40fc232253e/' },
@@ -82,7 +82,18 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
         ],
       },
       {
-        text: '更多',
+        text: '专升本',
+        link: '/more/',
+        items: [
+          { text: '学习', link: '/pages/f2a556/' },
+          { text: '面试', link: '/pages/aea6571b7a8bae86/' },
+          { text: '心情杂货', link: '/pages/2d615df9a36a98ed/' },
+          { text: '实用技巧', link: '/pages/baaa02/' },
+          { text: '友情链接', link: '/friends/' },
+        ],
+      },
+      {
+        text: '大学生活',
         link: '/more/',
         items: [
           { text: '学习', link: '/pages/f2a556/' },
@@ -334,7 +345,6 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
 
   // 监听文件变化并重新构建
   extraWatchFiles: [
-    '.vuepress/config.ts',
-    '.vuepress/config/htmlModules.ts',
+    '.vuepress/config.ts'
   ]
 })
