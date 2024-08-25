a8b51cca61afb861154fa3fab5fc5c4d1d28e0fc
RenYongXiang <2673741645@qq.com>
2024-08-25 17:06:30 +0800

    add notes

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -235,21 +235,6 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
 
   // 插件配置
   plugins: <UserPlugins>[
-    [
-      "sitemap", // 网站地图
-      {
-        hostname: WEB_SITE,
-      },
-    ],
-
-    'vuepress-plugin-baidu-autopush', // 百度自动推送
-
-    [
-      'vuepress-plugin-baidu-tongji', // 百度统计
-      {
-        hm: baiduCode,
-      },
-    ],
 
     // 全文搜索。 ⚠️注意：此插件会在打开网站时多加载部分js文件用于搜索，导致初次访问网站变慢。如在意初次访问速度的话可以不使用此插件！（推荐：vuepress-plugin-thirdparty-search）
     // 'fulltext-search',
@@ -275,10 +260,6 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
           {
             title: '在Bing中搜索',
             frontUrl: 'https://cn.bing.com/search?q=',
-          },
-          {
-            title: '通过百度搜索本站的',
-            frontUrl: `https://www.baidu.com/s?wd=site%3A${DOMAIN_NAME}%20`,
           },
         ],
       }
