a640954bcccfe8cbd0685a2fa650a68ad4eff7f9
RenYongXiang <2673741645@qq.com>
2024-08-25 16:51:05 +0800

    add notes

--- deploy.sh
+++ deploy.sh
@@ -3,22 +3,40 @@
 # 确保脚本抛出遇到的错误
 set -e
 
+# Windows系统下在提交代码时自动将LF转换为CRLF
+git config --global core.autocrlf true
 
-push_addr=`git remote get-url --push origin` # git提交地址，也可以手动设置，比如：push_addr=git@github.com:xugaoyi/vuepress-theme-vdoing.git
-commit_info=`git describe --all --always --long`
-dist_path=docs/.vuepress/dist # 打包生成的文件夹路径
-push_branch=gh-pages # 推送的分支
+# 文档源码提交到github
+git add -A
+git commit -m 'add notes'
+git pull --rebase origin main
+git push -u origin main
 
 # 生成静态文件
-npm run build
+npm run build:win
 
 # 进入生成的文件夹
-cd $dist_path
+cd docs/.vuepress/dist
+
+# 如果是发布到自定义域名
+# echo 'www.example.com' > CNAME
 
 git init
 git add -A
-git commit -m "deploy, $commit_info"
-git push -f $push_addr HEAD:$push_branch
+git commit -m 'deploy'
+
+
+
+git config --local user.name "RenYongXiang"
+git config --local user.email "2673741645@qq.com"
+
+# 如果发布到 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+ git push -f git@github.com:RenYongXiang/notesPage.git master:gh-pages
+
+# 如果使用 travis 持续集成
+#git push -f https://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@github.com/AngelingRemon/knowledgeNotes.git main:gh-pages
 
 cd -
-rm -rf $dist_path
--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -5,11 +5,6 @@ import { resolve } from 'path'
 import { defineConfig4CustomTheme, UserPlugins } from 'vuepress/config'
 import { VdoingThemeConfig } from 'vuepress-theme-vdoing/types'
 import dayjs from 'dayjs'
-import baiduCode from './config/baiduCode' // 百度统计hm码
-import htmlModules from './config/htmlModules' // 自定义插入的html块
-
-const DOMAIN_NAME = 'xugaoyi.com' // 域名 (不带https)
-const WEB_SITE = `https://${DOMAIN_NAME}` // 网址
 
 export default defineConfig4CustomTheme<VdoingThemeConfig>({
   theme: 'vdoing', // 使用npm主题包
@@ -119,7 +114,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
     ],
     sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
     logo: '/img/logo.png', // 导航栏logo
-    repo: 'xugaoyi/vuepress-theme-vdoing', // 导航栏右侧生成Github链接
+    // repo: 'xugaoyi/vuepress-theme-vdoing', // 导航栏右侧生成Github链接
     searchMaxSuggestions: 10, // 搜索结果显示最大数
     lastUpdated: '上次更新', // 开启更新时间，并配置前缀文字   string | boolean (取值为git提交时间)
     docsDir: 'docs', // 编辑的文件夹
@@ -166,15 +161,15 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
 
     // 文章默认的作者信息，(可在md文件中单独配置此信息) string | {name: string, link?: string}
     author: {
-      name: 'xugaoyi', // 必需
-      link: 'https://github.com/xugaoyi', // 可选的
+      name: 'RenYongXiang', // 必需
+      link: 'https://github.com/RenYongXiang', // 可选的
     },
 
     // 博主信息 (显示在首页侧边栏)
     blogger: {
       avatar: 'https://jsd.cdn.zzko.cn/gh/xugaoyi/image_store/blog/20200103123203.jpg',
-      name: 'Evan Xu',
-      slogan: '前端界的小学生',
+      name: 'RenYongXiang',
+      slogan: '大二新生',
     },
 
     // 社交图标 (显示于博主信息栏和页脚栏。内置图标：https://doc.xugaoyi.com/pages/a20ce8/#social)
@@ -184,12 +179,12 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
         {
           iconClass: 'icon-youjian',
           title: '发邮件',
-          link: 'mailto:894072666@qq.com',
+          link: 'mailto:2073741645@qq.com',
         },
         {
           iconClass: 'icon-github',
           title: 'GitHub',
-          link: 'https://github.com/xugaoyi',
+          link: 'https://github.com/RenYongXiang',
         },
         {
           iconClass: 'icon-erji',
@@ -209,13 +204,10 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
     // 扩展自动生成frontmatter。（当md文件的frontmatter不存在相应的字段时将自动添加。不会覆盖已有的数据。）
     extendFrontmatter: {
       author: {
-        name: 'xugaoyi',
-        link: 'https://github.com/xugaoyi'
+        name: 'RenYongXiang',
+        link: 'https://github.com/RenYongXiang'
       }
     },
-
-    // 自定义hmtl(广告)模块
-    htmlModules
   },
 
   // 注入到页面<head>中的标签，格式[tagName, { attrName: attrValue }, innerHTML?]
